58eecc5c33ac03ab85e2c74c375d414f3ee325d3
pierre furic <pfuric@hotmail.com>
2025-04-24 23:13:47 +0200

    dragtoparent improved

--- app/javascript/controllers/toggle_family_controller.js
+++ app/javascript/controllers/toggle_family_controller.js
@@ -4,7 +4,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["family"]
   connect() {
-    this.familyTarget.classList.add("d-none") // Masquer par défaut le submenu orders
+    this.familyTarget.classList.add("") // Masquer par défaut le submenu orders
     console.log('toggle_Family connected')
     console.log(this)
   }
